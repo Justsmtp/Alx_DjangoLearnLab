4201157c584b3d2b5a2735ee0eeac6c5ed09a961
Justsmtp <smtpwtf@gmail.com>
2025-07-14 19:02:25 +0100

    feat: implement user authentication with login, logout, and registration views and templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,3 +5,11 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
+
+from .views import register, CustomLoginView, CustomLogoutView
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+]
