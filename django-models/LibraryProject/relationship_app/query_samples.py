7ee3c8ac2f7f76f1b79cca156d8085b7431b748b
Justsmtp <smtpwtf@gmail.com>
2025-07-14 18:27:50 +0100

    feat: add relationship_app with complex model relationships and sample queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,22 +1,28 @@
 import os
 import django
 
-# Setup environment
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+# Setup Django environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
-# 1. Query all books by a specific author
-author = Author.objects.get(name='Chinua Achebe')
+# Query all books by a specific author
+author_name = "Chinua Achebe"
+author = Author.objects.get(name=author_name)
 books_by_author = author.books.all()
-print(f"Books by {author.name}: {[book.title for book in books_by_author]}")
+print(f"Books by {author_name}:")
+for book in books_by_author:
+    print(book.title)
 
-# 2. List all books in a library
-library = Library.objects.get(name='Central Library')
-library_books = library.books.all()
-print(f"Books in {library.name}: {[book.title for book in library_books]}")
+# List all books in a library
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+print(f"\nBooks in {library_name}:")
+for book in books_in_library:
+    print(book.title)
 
-# 3. Retrieve the librarian for a library
+# Retrieve the librarian for a library
 librarian = library.librarian
-print(f"Librarian of {library.name}: {librarian.name}")
+print(f"\nLibrarian of {library_name}: {librarian.name}")
