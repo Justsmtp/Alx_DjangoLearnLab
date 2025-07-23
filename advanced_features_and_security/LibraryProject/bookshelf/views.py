02567aa7c5f1de6eeaf704fef04570f613287580
Justsmtp <smtpwtf@gmail.com>
2025-07-23 22:10:36 +0100

    chore(security): enforce security best practices (CSRF, CSP, secure settings, ORM)

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import permission_required
 from .models import Book
 from .forms import BookForm
 from django.db.models import Q
+from .forms import ExampleForm
+
 
 
 @permission_required('bookshelf.can_view', raise_exception=True)
