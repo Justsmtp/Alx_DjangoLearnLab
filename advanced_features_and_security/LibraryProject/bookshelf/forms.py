f45d8cbde44931b532f57b3a9e822952edeb5f97
Justsmtp <smtpwtf@gmail.com>
2025-07-23 21:54:05 +0100

    chore(security): enforce security best practices (CSRF, CSP, secure settings, ORM)

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -8,3 +8,9 @@ class BookForm(forms.ModelForm):
 
 class SearchForm(forms.Form):
     q = forms.CharField(max_length=100)
+
+ 
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book  
+        fields = ['title', 'author', 'published_date']  
