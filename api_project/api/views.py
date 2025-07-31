249291f6307a669fbd8e705856becc082fbbff4a
Justsmtp <smtpwtf@gmail.com>
2025-08-01 00:17:07 +0100

    feat(api): add API endpoint to list all books using DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import generics
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
