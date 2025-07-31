b5aaf3cc4c12caf0af65825c5965225507d53d7b
Justsmtp <smtpwtf@gmail.com>
2025-08-01 00:21:06 +0100

    feat(api): add BookViewSet with CRUD operations using DRF routers

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.db import router
+from django.urls import path, include
 from .views import BookList
+
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+
+    path('', include(router.urls)),
 ]
