44ca6c83e0e3d9edefdad9052f68dca0cabff811
Justsmtp <smtpwtf@gmail.com>
2025-08-13 19:15:17 +0100

    Implement class-based views for Posts and Comments, including CRUD operations, user registration, and profile management.

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -33,3 +33,19 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+
+from taggit.forms import TagWidget
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
