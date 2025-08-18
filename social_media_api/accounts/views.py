913e06c133b36d4e6104ca34c544bd712a5873ef
Justsmtp <smtpwtf@gmail.com>
2025-08-18 16:12:15 +0100

    Implement follow and unfollow functionality in accounts app

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,39 +1,46 @@
-from urllib import response
-from rest_framework import generics, permissions
 from django.shortcuts import get_object_or_404
+from rest_framework import generics, permissions
+from rest_framework.response import Response
 from .models import CustomUser
 from .serializers import UserSerializer
 
-
+# Follow a user
 class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
 
     def post(self, request, user_id):
-        target_user = get_object_or_404(CustomUser.objects.all(), id=user_id)
-        request.user.follow(target_user)
-        return response({"detail": f"You are now following {target_user.username}"})
+        user_to_follow = get_object_or_404(CustomUser.objects.all(), id=user_id)
+        request.user.follow(user_to_follow)
+        return Response({"detail": f"You are now following {user_to_follow.username}"})
 
 
+# Unfollow a user
 class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
 
     def post(self, request, user_id):
-        target_user = get_object_or_404(CustomUser.objects.all(), id=user_id)
-        request.user.unfollow(target_user)
-        return response({"detail": f"You unfollowed {target_user.username}"})
+        user_to_unfollow = get_object_or_404(CustomUser.objects.all(), id=user_id)
+        request.user.unfollow(user_to_unfollow)
+        return Response({"detail": f"You have unfollowed {user_to_unfollow.username}"})
 
 
+# List who the current user is following
 class FollowingListView(generics.ListAPIView):
-    serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
 
     def get_queryset(self):
         return self.request.user.following.all()
 
 
+# List followers of the current user
 class FollowersListView(generics.ListAPIView):
-    serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
 
     def get_queryset(self):
         return self.request.user.followers.all()
