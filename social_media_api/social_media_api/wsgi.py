694fd8183db35702f0f42174f03a34ea162faadb
Justsmtp <smtpwtf@gmail.com>
2025-08-24 00:33:33 +0100

    Prepare project for PythonAnywhere deployment

--- social_media_api/social_media_api/wsgi.py
+++ social_media_api/social_media_api/wsgi.py
@@ -14,12 +14,18 @@ WSGI config for social_media_api project on PythonAnywhere.
 import os
 import sys
 
-# Add project root to Python path
-project_home = '/home/justsmtp/Django_Framework/social_media_api'
-if project_home not in sys.path:
-    sys.path.append(project_home)
+# Add your project directory to the sys.path
+path = '/home/Justsmtp/Alx_DjangoLearnLab/social_media_api'
+if path not in sys.path:
+    sys.path.append(path)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_api.settings')
+# Set the settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'social_media_api.settings'
+
+# Activate virtualenv
+activate_this = '/home/Justsmtp/Alx_DjangoLearnLab/social_media_api/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
